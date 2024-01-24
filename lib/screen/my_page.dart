61fe9e312edecb13aab926b1d723ad0d2709c533
Meri11Ra1 <r.t.00293794@gmail.com>
2024-01-24 21:01:28 +0900

    show posted image in my page

--- lib/screen/my_page.dart
+++ lib/screen/my_page.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 
 class Styles {
   static ButtonStyle setRadiusValue(double value) {
-    return (   
-      OutlinedButton.styleFrom(
+    return (OutlinedButton.styleFrom(
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(value),
-        )
-      )
-    );
+      borderRadius: BorderRadius.circular(value),
+    )));
   }
 
   static const mediumTextBold = TextStyle(
@@ -22,90 +19,111 @@ class MyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('マイページ'),
-          centerTitle: true,
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Image.network(
-                  'https://i1.wp.com/www.globemart.co.jp/newwp/wp-content/uploads/2020/07/png-transparent-instagram-application-logo-logo-computer-icons-instagram-miscellaneous-text-trademark.png',
-                  width: 70,
-                  height: 70,
-                ),
-                const Spacer(),
-                const SizedBox(width: 16),
-                const Column(
-                  children: [
-                    Text(
-                      '7,041',
-                      style: Styles.mediumTextBold
-                    ),
+    return Scaffold(
+      appBar: AppBar(title: const Text('マイページ'), centerTitle: true),
+      body: SingleChildScrollView(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.network(
+                    'https://static-00.iconduck.com/assets.00/instagram-icon-1024x1024-8qt57uwd.png',
+                    height: 80,
+                    width: 80,
+                  ),
+                  const Spacer(),
+                  const Column(children: [
+                    Text('7,041', style: Styles.mediumTextBold),
                     Text('投稿'),
-                  ]
-                ),
-                const SizedBox(width: 16),
-                const Column(
-                  children: [
-                    Text(
-                      '4.6億',
-                      style: Styles.mediumTextBold
-                    ),
+                  ]),
+                  const SizedBox(width: 16),
+                  const Column(children: [
+                    Text('4.6億', style: Styles.mediumTextBold),
                     Text('フォロワー'),
-                  ]
-                ),
-                const SizedBox(width: 16),
-                const Column(
-                  children: [
-                    Text(
-                      '99',
-                      style: Styles.mediumTextBold
-                    ),
+                  ]),
+                  const SizedBox(width: 16),
+                  const Column(children: [
+                    Text('99', style: Styles.mediumTextBold),
                     Text('フォロー'),
-                  ]
-                )
-              ]
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'Instagram',
-              style: Styles.mediumTextBold,
-            ),
-            const Text(
-              '#YoursToMake',
-              style: TextStyle(color: Colors.blue),
-            ),
-            const Text(
-              'help.instagram.com',
-              style: TextStyle(color: Colors.blue),
-            ),
-            Row(
+                  ])
+                ]),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(
+                'Instagram',
+                style: Styles.mediumTextBold,
+              ),
+              Text(
+                '#YoursToMake',
+                style: TextStyle(color: Colors.blue),
+              ),
+              Text(
+                'help.instagram.com',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ]),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
               children: [
-                OutlinedButton(
-                  onPressed: () => {},
-                  child: Text('フォロー中'),
-                  style: Styles.setRadiusValue(8.0)
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: OutlinedButton(
+                            onPressed: () => {},
+                            style: Styles.setRadiusValue(8.0),
+                            child: const Text(
+                              'フォロー中',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12),
+                            )),
+                      ),
+                      const SizedBox(width: 4),
+                      Expanded(
+                        child: OutlinedButton(
+                            onPressed: () => {},
+                            style: Styles.setRadiusValue(8.0),
+                            child: const Text(
+                              'メッセージ',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14),
+                            )),
+                      ),
+                    ],
+                  ),
                 ),
+                const SizedBox(width: 4),
                 OutlinedButton(
-                  onPressed: () => {},
-                  child: Text('メッセージ'),
-                  style: Styles.setRadiusValue(8.0)
-                ),
-                OutlinedButton(
-                  onPressed: () => {},
-                  child: Icon(Icons.keyboard_arrow_down),
-                  style: Styles.setRadiusValue(8.0)
-                )
+                    onPressed: () => {},
+                    style: Styles.setRadiusValue(8.0),
+                    child: const Icon(Icons.keyboard_arrow_down,
+                        color: Colors.black))
               ],
-            )
-          ]
-        ),
+            ),
+          ),
+          GridView.count(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            primary: false,
+            crossAxisCount: 3,
+            children: <Widget>[
+              Image.network(
+                'https://static-00.iconduck.com/assets.00/instagram-icon-1024x1024-8qt57uwd.png',
+              ),
+            ],
+          )
+        ]),
       ),
     );
   }

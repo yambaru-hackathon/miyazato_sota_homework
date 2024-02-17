44817d3951516db588cbc8082bf8566d59c671db
Meri11Ra1 <R.t.00293794@gmail.com>
2024-02-17 15:03:22 +0900

    firebase CRUD

---
+++ lib/user.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  User({
+    required this.id,
+    required this.first,
+    required this.last,
+    required this.born,
+  });
+
+  final String id;
+  final String first;
+  final String last;
+  final String born;
+
+  factory User.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return User(
+      id: doc.id,
+      first: data['first'],
+      last: data['last'],
+      born: data['born'],
+    );
+  }
+}
